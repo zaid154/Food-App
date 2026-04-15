bca8d7cdd4811d8f40baa8e898475c816d36a373
Mohd Zaid <mohd.zaid.620268@gmail.com>
2026-04-15 20:36:00 +0530

    update the code with jest

--- src/Component/AddToCard.js
+++ src/Component/AddToCard.js
@@ -1,10 +1,18 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
 
-const Order_Cart = () => {
-    return (
-        <div className="abc">
-            <h1>this is Cart us page</h1>
-        </div>
-    )
-};
+const AddToCarts = () => {
+    const allCart= useSelector((data) => data.cart.items)
+     console.log(allCart);
 
-export default Order_Cart;
+  return (
+      <div>
+        {allCart.map(item=><div className='bg-white h-14 flex justify-center items-center'>
+          <h1>{item.name}</h1>
+          
+        </div>)}
+      </div>
+  )
+}
+
+export default AddToCarts
